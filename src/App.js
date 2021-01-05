cc718d085158cac19290f104938947a5e672d891
Kyle Swaffield <kyle@swaff.id.au>
2021-01-06 10:15:48 +1100

    user-button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-// import SeatSelection from './SeatSelection';
-// import Home from './Home';
+import SeatSelection from './SeatSelection';
+import Home from './Home';
 import UserButton from './components/UserButton';
 
 function App() {
   return (
     <div className="App">
       <UserButton />
+      <SeatSelection />
+      <Home />
     </div>
   );
 }
