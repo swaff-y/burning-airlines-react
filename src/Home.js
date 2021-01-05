330ffdf0388f5809666e2675247254bcded6313a
david-Toledo <davidtoledo87@hotmail.com>
2021-01-05 23:07:33 +1100

    testing home

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import axios from 'axios';
-import '../App.css';
+import './App.css';
 
 
-const BASE_URL = 'http://localhost:3000/secrets';
+const BASE_URL = 'http://localhost:3000/';
 
-class Home extends from React.Component {
+class Home extends React.Component {
 
       render(){
       return(
+        <div className="App">
+          Testing Testing ... 123
+
+        </div>
 
       ) //return
     } //render
 
   };
 
-}
+
 
 export default Home
