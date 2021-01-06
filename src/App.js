4af2b0ef0d2cff12396852b9db6c21d3adb91c98
david-Toledo <davidtoledo87@hotmail.com>
2021-01-06 13:25:13 +1100

    selected flight

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import SeatSelection from './SeatSelection';
 import Home from './Home';
 import UserButton from './components/UserButton';
 import SearchForm from './components/SearchForm';
+import SelectedFlight from './SelectedFlight';
 
 function App() {
+  setState={
+    selectedFlight:[]
+  }
+
   return (
     <div className="App">
-     <SearchForm />
+     <SearchForm selectedFlight={this.state.selectedFlight}/>
      <UserButton />
      <Home />
      <SeatSelection />
