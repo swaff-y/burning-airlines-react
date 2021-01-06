dd421c50c3087365ddf5bcd3a5c343747f49fc73
Kyle Swaffield <kyle@swaff.id.au>
2021-01-07 02:00:54 +1100

    seat-selection

--- src/components/FlightDetails.js
+++ src/components/FlightDetails.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import SeatSelection from './SeatSelection';
 import SeatSelector from './SeatSelector';
+import SelectedFlight from './SelectedFlight';
+import axios from 'axios';
 
+const FLIGHTS_API_URL = 'http://localhost:3000/flights/select/';
 
 class FlightDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         data: [],
-        flightId: 68
+        flightId: 68,
+        flight: [],
+        col: 0,
+        row: 0,
+        seatPicked: "",
       };
   };
+
+  handleSeatSelected = (seat) => {
+    console.log("Handle seleted", seat);
+  }
+  handleSeatPicked = (seatPicked) =>{
+    this.setState({seatPicked})
+  }
+
+  componentDidMount = () => {
+    const url = FLIGHTS_API_URL + this.props.match.params.flight_id
+    console.log("thisone:", url);
+    axios.get(url)
+    .then( res => {
+      console.log('response: ', res.data.airplane.row);
+      this.setState({row:res.data.airplane.row})
+      this.setState({col:res.data.airplane.column})
+      this.setState({flight: res.data}); //save the response into state
+    })
+  }
 
   render() {
     return (
       <div>
-        <SeatSelection flightId={this.state.flightId}  />
-        <SeatSelector handleSeatSelected={this.props.handleSeatSelected} />
+        <SelectedFlight flightId={this.props.match.params.flight_id} />
+        <SeatSelection flightId={this.props.match.params.flight_id}  row={this.state.row} col={this.state.col} handleSeatPicked={this.handleSeatPicked}/>
+        <SeatSelector handleSeatSelected={this.handleSeatSelected} seatPicked={this.state.seatPicked}/>
       </div>
     );
   }
--- src/components/SelectedFlight.js
+++ src/components/SelectedFlight.js
@@ -16,30 +16,42 @@ class SelectedFlight extends React.Component {
 
   componentDidMount(props){
 
-    let URL = AIRPLANE_API_URL+this.props.selectedFlight
+    let URL = AIRPLANE_API_URL+this.props.flightId
      // console.log(URL);
     axios.get(URL)
     .then((res)=>{
-    console.log('response:', res.data);
+    // console.log('response:', res.data);
     this.setState({selectedFlight:res.data}); //save into state
     })
     .catch(console.warn);
   } //componentDidMount
 
   render() {
-    let flight = this.state.selectedFlight
+    let flight = this.state.selectedFlight;
     return (
     <div className="App">
-      <h2> Flight {flight.flight_no} </h2>
-      <p>
-        <strong>Date: </strong> {flight.date } <strong> Flight no.: </strong> {flight.flight_no} <strong> From: </strong> {flight.from} <strong> -&gt; To:</strong> {flight.to}
-      </p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Date:</th>
+            <th>Flight No:</th>
+            <th>From:</th>
+            <th>To:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{flight.date}</td>
+            <td>{flight.flight_no}</td>
+            <td>{flight.from}</td>
+            <td>{flight.to}</td>
+          </tr>
+        </tbody>
+      </table>
 
     </div>
     ); // return
   }; //render()
 };
-
-
 
 export default SelectedFlight
