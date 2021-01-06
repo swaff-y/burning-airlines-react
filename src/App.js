d5fb83c0eefbb20d891bce59a1b94af385171276
Kyle Swaffield <kyle@swaff.id.au>
2021-01-06 13:17:36 +1100

    app-classComponent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import React from 'react';
 import './App.css';
-import SeatSelection from './SeatSelection';
-import Home from './Home';
-import UserButton from './components/UserButton';
-import SearchForm from './components/SearchForm';
+// import SeatSelection from './SeatSelection';
+// import Home from './Home';
+// import UserButton from './components/UserButton';
+// import SearchForm from './components/SearchForm';
+ import SeatSelector from './components/SeatSelector';
 
-function App() {
-  return (
-    <div className="App">
-     <SearchForm />
-     <UserButton />
-     <Home />
-     <SeatSelection />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    seatSelection: '4B',
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <SeatSelector seatSelection={this.state.seatSelection}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+// <SearchForm />
 // <UserButton />
 // <Home />
 // <SeatSelection />
