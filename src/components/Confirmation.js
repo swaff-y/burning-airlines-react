d8ce1b7d46e90f770303a02b2f3ccba108945f09
Kyle Swaffield <kyle@swaff.id.au>
2021-01-07 13:48:22 +1100

    semi-working

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -3,21 +3,14 @@ import '../App.css';
 import axios from 'axios';
 import { Route, Link, HashRouter as Router} from 'react-router-dom';
 
-// import SeatSelection from './SeatSelection';
-// import Home from './Home';
-// import UserButton from './UserButton';
-// import SearchForm from './SearchForm';
-// import SeatSelector from './SeatSelector';
-// import SelectedFlight from './SelectedFlight';
-
-const RESERVATION_API_URL = 'http://localhost:3000/reservations/select/';
+const RESERVATION_API_URL = 'http://localhost:3000/reservations/booking/';
 
 class Confirmation extends React.Component {
 
   constructor(props) {
       super(props);
       this.state = {
-          confirmationCode: "34",
+          // confirmationData: [],
           flightNumber: "",
           flightTo:"",
           flightFrom:"",
@@ -30,11 +23,12 @@ class Confirmation extends React.Component {
     //===============================================================
     //this is currently pulling flight ID ..just to confirm API pull is working.
     //===============================================================
-    let URL = RESERVATION_API_URL+this.state.confirmationCode
-     // console.log(URL);
+    let URL = RESERVATION_API_URL+this.props.match.params.reservationid
+      console.log(URL);
     axios.get(URL)
     .then((res)=>{
     console.log('response:', res.data)
+    this.setState({confirmationData:res.data})
     this.setState({flightNumber:res.data[0].flight.flight_no}) //save into state
     this.setState({flightTo:res.data[0].flight.to}) //save into state
     this.setState({flightFrom:res.data[0].flight.from}) //save into state
@@ -45,21 +39,25 @@ class Confirmation extends React.Component {
   } //componentDidMount
 
   render(){
+    console.log("user: ", this.state.confirmationData);
+
     return (
       <div className="container">
-        <h1>Thanks for booking with Burnt Airlines</h1>
-        <h4>Your confirmation code is: {this.state.confirmationCode}</h4>
+        <h1>Thanks for booking with Burnt Airlines. Please check your emails for the confimation email that you will never recieve.</h1>
+        <h4>Your confirmation code is: {this.props.match.params.reservationid}</h4>
         <br />
         <hr />
         < br />
         <h3>Your booking details: </h3>
         <br />
-        <ul className="list-group">
-          <li className="list-group-item">Passenger name: {this.state.name}</li>
-          <li className="list-group-item">Flying from <em><strong>{this.state.flightFrom}</strong></em>, to <em><strong>{this.state.flightTo}</strong></em></li>
-          <li className="list-group-item">Date: {this.state.flightDay}</li>
-          <li className="list-group-item">Flight number: {this.state.flightNumber}</li>
-        </ul>
+          <ul className="list-group">
+            <li className="list-group-item">Passenger Name:
+              {this.state.name}</li>
+            <li className="list-group-item">Flying from <em><strong>{this.state.flightFrom}</strong></em>, to <em><strong>{this.state.flightTo}</strong></em></li>
+            <li className="list-group-item">Date: {this.state.flightDay}</li>
+            <li className="list-group-item">Flight number: {this.state.flightNumber}</li>
+          </ul>
+
         <br />
 
       <p><Link className="btn btn-primary"to="/search">Book another flight</Link></p>
@@ -72,3 +70,11 @@ class Confirmation extends React.Component {
 } //Confirmation class
 
 export default Confirmation;
+
+// <ul className="list-group">
+//   <li className="list-group-item">Passenger Name:
+//     {this.state.name}</li>
+//   <li className="list-group-item">Flying from <em><strong>{this.state.confirmationData.flight.from}</strong></em>, to <em><strong>{this.state.confirmationData.flight.to}</strong></em></li>
+//   <li className="list-group-item">Date: {this.state.confirmationData.flight.date}</li>
+//   <li className="list-group-item">Flight number: {this.state.confirmationData.flight.flight_no}</li>
+// </ul>
--- src/components/SeatSelector.js
+++ src/components/SeatSelector.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Route, Link, HashRouter as Router} from 'react-router-dom';
+import {Route, Link, HashRouter as Router} from 'react-router-dom';
 import axios from 'axios';
 
 const RESERVATION_POST_URL = 'http://localhost:3000/reservations';
@@ -9,29 +9,29 @@ class SeatSelector extends React.Component {
       super(props);
       this.state = {
           seatSelection: '',
+          bookingCode: '',
       };
   };
 
-  bookingCode = () => {
+  componentDidMount(){
     const randomCode = Math.floor(Math.random()*16777215).toString(16);
-    return randomCode;
+     this.setState({bookingCode:randomCode});
   }
 
   handleClick = (ev) => {
-    console.log("clicked value:", ev.target.value);
+    console.log("clicked value:", ev.target.innerHTML);
     console.log("URL:", RESERVATION_POST_URL);
 
     // const bookingCode = bookingCode();
 
     axios.post(RESERVATION_POST_URL, {
       seat_no: ev.target.value,
-      booking_code: this.bookingCode(),
+      booking_code: this.state.bookingCode,
       flight_id: this.props.flightId,
       user_id: this.props.userId,
     })
     .then((res)=>{
     console.log('response:', res.data);
-    // <Link to="">
     // this.setState({selectedFlight:res.data}); //save into state
     })
     .catch(console.warn);
@@ -40,14 +40,13 @@ class SeatSelector extends React.Component {
 
   render() {
     return (
-
     <div className="container">
       <div className="row justify-content-center">
         <div className="text-align-right  col-2">
         <strong>{ this.props.seatPicked }</strong>
         </div>
         <div className="text-align-left col-2">
-          <button className="btn btn-primary" value={this.props.seatPicked} onClick={this.handleClick}>Select Seat</button>
+          <Link to={"/confirmation/" + this.state.bookingCode}><button className="btn btn-primary" value={this.props.seatPicked} onClick={this.handleClick}>Select Seat</button></Link>
         </div>
       </div>
     </div>
